c96ce1a567d8101708edd012293372657f0df1c7
Aleksandar Vacić <av@radianttap.com>
2019-04-13 12:28:14 +0200

    Services will contains wrappers for web services. First in the list is Annanow. This service will be the only module to communicate with Annanow web service and should be written entirely independent from Shop app, meaning it could be extended later to be the same across all apps (and thus at some point convert it into framework).
    It has not knowledge about Data Model of the app and only communicates using Foundation and Standard library types (JSON and friends).

---
+++ Services/Annanow/AnnanowService.swift
@@ -0,0 +1,13 @@
+//
+//  AnnanowService.swift
+//  AnnanowShop
+//
+//  Created by Aleksandar Vacić on 4/13/19.
+//  Copyright © 2019 Annanow. All rights reserved.
+//
+
+import Foundation
+
+final class Annanow {
+	
+}
