1950c8f65e16c236961b6c37bb42bdf0968100a9
Aleksandar Vacić <av@radianttap.com>
2019-04-13 12:22:22 +0200

    DataModel contains just base declarations for app's data types used across DataManager, middleware and UI Also contains app-wide extensions, like Codable conformances, maybe shared property formatting etc

---
+++ DataModel/Common.swift
@@ -0,0 +1,18 @@
+//
+//  Common.swift
+//  AnnanowShop
+//
+//  Created by Aleksandar Vacić on 4/13/19.
+//  Copyright © 2019 Annanow. All rights reserved.
+//
+
+/**
+	Common shared types,
+	usually typealiases for system stuff, to make them more readavle and obvious what they do.
+
+*/
+
+import Foundation
+
+public typealias JSON = [String: Any]
+
