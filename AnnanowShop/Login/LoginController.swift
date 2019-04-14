123bbbaceac1cff9d50922b1ee0d6cac9aa1dde4
Aleksandar Vacić <av@radianttap.com>
2019-04-14 12:37:50 +0200

    Created Login UIViewController subclass. Steps:
    - create the .swift file
    - make the class final
    - adopt StoryboardLoadable protocol
    
    Name the storyboard file same as controller.
    
    - set class
    - copy class name into Storyboard ID
    - (optionally) make the UIVC instance the initial View Controller
    
    Now it's one-liner to load this UIVC from storyboard: LoginController.instantiate()

---
+++ AnnanowShop/Login/LoginController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginController.swift
+//  AnnanowShop
+//
+//  Created by Aleksandar Vacić on 4/14/19.
+//  Copyright © 2019 Annanow. All rights reserved.
+//
+
+import UIKit
+
+final class LoginController: UIViewController, StoryboardLoadable {
+}
