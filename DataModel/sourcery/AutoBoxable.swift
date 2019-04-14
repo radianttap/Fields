6d18de4b48bb236781c7855e3927d22e65c4bc98
Aleksandar Vacić <av@radianttap.com>
2019-04-14 19:52:56 +0200

    Added sorcery template to auto-generate *Box types,
    needed for types used in coordinatingResponder methods.
    
    Just adopt AutoBoxable protocol and build.

---
+++ DataModel/sourcery/AutoBoxable.swift
@@ -0,0 +1,15 @@
+//
+//  AutoBoxable.swift
+//  AnnanowShop
+//
+//  Created by Aleksandar Vacić on 4/13/19.
+//  Copyright © 2019 Annanow. All rights reserved.
+//
+
+import Foundation
+
+///	Apply to all non-ObjC data types that should pass-through
+///	as arguments in coordinatingResponder methods
+protocol AutoBoxable {}
+
+
