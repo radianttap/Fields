d68c65ac3bf81be5da7e12216e8e37f3549f5c98
Aleksandar Vacić <av@radianttap.com>
2019-04-14 20:02:20 +0200

    Login controller data flows implemented
    UserResult is the only input.
    Output is either login submit or switch to forgot-password.

--- AnnanowShop/Login/LoginController.swift
+++ AnnanowShop/Login/LoginController.swift
@@ -9,4 +9,53 @@
 import UIKit
 
 final class LoginController: UIViewController, StoryboardLoadable {
+
+	//	INPUT
+	var userResult: UserResult? {
+		didSet {
+			if !isViewLoaded { return }
+			process(userResult)
+		}
+	}
+}
+
+extension LoginController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		process(userResult)
+	}
+}
+
+private extension LoginController {
+	@IBAction func openForgotPassword(_ sender: UIButton) {
+		//	OUTPUT
+		accountDisplayForgotPassword(sender: self)
+	}
+
+	func process(_ userResult: UserResult?) {
+		guard let userResult = userResult else {
+			return
+		}
+
+		switch userResult {
+		case .success(let user):
+			print(user)
+
+		case .failure(let accountError):
+			print(accountError)
+		}
+	}
+
+	func submit() {
+		//	validate input
+		let user: String = ""
+		let pass: String = ""
+
+		//	OUTPUT
+		accountPerformLogin(username: user, password: pass, onQueue: .main, sender: self) {
+			[weak self] boxedUserResult in
+			self?.process(boxedUserResult.unbox)
+		}
+	}
 }
