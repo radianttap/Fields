755968dd8dd9228807d04ebb0aac024e8ef9d057
Aleksandar Vacić <av@radianttap.com>
2019-04-15 10:13:08 +0200

    Custom User-Agent for the app, including name, version & build, plus device and iOS details.
    If Annanow service keeps the access logs, this could be really helpful after few years in production, to figure out what devices and OS version are customer using.

--- Services/Annanow/AnnanowService.swift
+++ Services/Annanow/AnnanowService.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2019 Annanow. All rights reserved.
 //
 
+#if os(watchOS)
 import Foundation
+#else
+import UIKit
+#endif
 
 final class Annanow {
 
@@ -16,3 +20,28 @@ final class Annanow {
 }
 
 extension Annanow: Loggable {}
+
+private extension Annanow {
+	///	Headers, added by URLSession, to each URLRequest instance
+	static let commonHeaders: [String: String] = {
+		return [
+			"User-Agent": userAgent,
+			"Accept-Charset": "utf-8",
+			"Accept-Encoding": "gzip, deflate"
+		]
+	}()
+
+	static var userAgent: String = {
+		#if os(watchOS)
+		let osName = "watchOS"
+		let osVersion = ""
+		let deviceVersion = "Apple Watch"
+		#else
+		let osName = UIDevice.current.systemName
+		let osVersion = UIDevice.current.systemVersion
+		let deviceVersion = UIDevice.current.model
+		#endif
+		let locale = Locale.current.identifier
+		return "\( Bundle.appName ) \( Bundle.appVersion ) (\( Bundle.appBuild )); \( deviceVersion ); \( osName ) \( osVersion ); \( locale )"
+	}()
+}
