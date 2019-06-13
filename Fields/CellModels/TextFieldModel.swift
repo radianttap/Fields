68e684e55bdbda4c1ace6cb906a3c346ccb12d34
Aleksandar Vacić <av@radianttap.com>
2019-06-13 10:54:26 +0200

    Each instance of any type of cell may need some custom setup.
    When you look at username/password fields - they are both TextFieldCells, but their setup is vastly different. Who knows what else may be needed. Thus each Cell/Model pair should expose the ability to customize the core of the cell (the UIControl) as you need.

--- Fields/CellModels/TextFieldModel.swift
+++ Fields/CellModels/TextFieldModel.swift
@@ -6,14 +6,24 @@
 //  MIT License · http://choosealicense.com/licenses/mit/
 //
 
-import Foundation
+import UIKit
 
-struct TextFieldModel {
+struct TextFieldModel: FieldModel {
+	let id: String
+
 	var title: String?
 	var value: String?
 	var placeholder: String?
+	var customSetup: (UITextField) -> Void
 
-	init() {
+	init(id: String, title: String? = nil, value: String? = nil, placeholder: String? = nil, customSetup: @escaping (UITextField) -> Void = {_ in}) {
+		self.id = id
+
+		self.title = title
+		self.value = value
+		self.placeholder = placeholder
+
+		self.customSetup = customSetup
 	}
 }
 
