14596e7e60538c9b19b0b69c065c5bfec5287ef1
Aleksandar Vacić <av@radianttap.com>
2019-06-13 11:13:59 +0200

    Simple model object, that you may have in an app

---
+++ demo-app/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Fields-demo
+//
+//  Created by Aleksandar Vacić on 6/12/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+	var username: String?
+	var password: String?
+}
