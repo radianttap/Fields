637664c1aa6eb2c0efe332a9d6f81abf4bd76791
Aleksandar Vacić <av@radianttap.com>
2019-06-13 10:47:05 +0200

    We need a way to connect our own code to the field specific model.
    All we need is some sort of ID, in order to recognize the field.

---
+++ Fields/CellModels/FieldModel.swift
@@ -0,0 +1,13 @@
+//
+//  FieldModel.swift
+//  Fields-demo
+//
+//  Created by Aleksandar Vacić on 6/12/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+import Foundation
+
+protocol FieldModel {
+	var id: String { get }
+}
