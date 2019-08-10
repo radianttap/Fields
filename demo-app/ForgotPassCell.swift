f8bc7e271b2185e0abc6b998b89f24188820ea57
Aleksandar Vacić <av@radianttap.com>
2019-08-10 12:14:43 +0200

    Forgot password cell, used on Login VC

---
+++ demo-app/ForgotPassCell.swift
@@ -0,0 +1,12 @@
+//
+//  ForgotPassCell.swift
+//  Fields-demo
+//
+//  Created by Aleksandar Vacić on 8/10/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+final class ForgotPassCell: FieldCell, NibReusableView {
+}
