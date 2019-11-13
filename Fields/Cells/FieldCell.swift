758f962a06553261d1d88c892ae5776e13a2309f
Aleksandar Vacić <av@radianttap.com>
2019-11-13 11:04:16 +0100

    I have no idea why is this needed in iOS 12 (at least).

--- Fields/Cells/FieldCell.swift
+++ Fields/Cells/FieldCell.swift
@@ -13,6 +13,13 @@ class FieldCell: UICollectionViewCell {
 	override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
 		let attr = layoutAttributes.copy() as! UICollectionViewLayoutAttributes
 
+		if #available(iOS 13, *) {
+			//	works fine
+		} else {
+			//	without this, fittedSize (below) becomes {0,0}
+			layoutIfNeeded()
+		}
+
 		let fittedSize = systemLayoutSizeFitting(UIView.layoutFittingCompressedSize,
 												 withHorizontalFittingPriority: UILayoutPriority.fittingSizeLevel,
 												 verticalFittingPriority: UILayoutPriority.fittingSizeLevel)
