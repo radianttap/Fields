a84cd18fe874b65496babff87564a1e155ee6747
Aleksandar Vacić <av@radianttap.com>
2019-11-20 19:14:47 +0100

    Make sure the cells fit inside safe area for the view

--- Fields/Layouts/FullWidthLayout.swift
+++ Fields/Layouts/FullWidthLayout.swift
@@ -7,6 +7,8 @@ import UIKit
 class FullWidthLayout: BaseGridLayout {
 
 	override func prepare() {
+		adjustSectionInsetsForSafeArea()
+
 		if let cv = collectionView {
 			let w = cv.bounds.width - (sectionInset.left + sectionInset.right)
 			itemSize.width = max(w, 0)
--- Fields/Layouts/GridLayout.swift
+++ Fields/Layouts/GridLayout.swift
@@ -25,10 +25,34 @@ class BaseGridLayout: UICollectionViewFlowLayout {
 		scrollDirection = .vertical
 		headerReferenceSize = .zero
 		footerReferenceSize = .zero
-		minimumLineSpacing = 0
+		minimumLineSpacing = 8
 		minimumInteritemSpacing = 0
 		sectionInset = .zero
 		itemSize = CGSize(width: 260, height: 50)
+	}
+
+	func adjustSectionInsetsForSafeArea() {
+		guard let cv = collectionView else { return }
+
+		sectionInset = cv.safeAreaInsets
+
+		switch scrollDirection {
+		case .horizontal:
+			sectionInset.left = 0
+			sectionInset.right = 0
+
+		case .vertical:
+			sectionInset.top = 0
+			sectionInset.bottom = 0
+
+		@unknown default:
+			sectionInset = .zero
+		}
+	}
+
+	override func prepare() {
+		adjustSectionInsetsForSafeArea()
+		super.prepare()
 	}
 
 	override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
