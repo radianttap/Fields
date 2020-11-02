0f5685cd3a844336eb7385f7f965bd99e271a017
Aleksandar Vacić <av@radianttap.com>
2020-11-02 13:02:46 +0100

    Allow to set baseInsets for all fields, which is then adjusted for safe area as well

--- Fields/Layouts/GridLayout.swift
+++ Fields/Layouts/GridLayout.swift
@@ -5,6 +5,8 @@
 import UIKit
 
 class BaseGridLayout: UICollectionViewFlowLayout {
+	///	Additional padding around the sections, which is visually added to existing layoutMargins (which is also adjusted for safeArea) of the CV
+	open var baseSectionInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
 
 	override func awakeFromNib() {
 		super.awakeFromNib()
@@ -27,27 +29,29 @@ class BaseGridLayout: UICollectionViewFlowLayout {
 		footerReferenceSize = .zero
 		minimumLineSpacing = 8
 		minimumInteritemSpacing = 0
-		sectionInset = .zero
+		sectionInset = baseSectionInset
 		itemSize = CGSize(width: 260, height: 50)
 	}
 
 	func adjustSectionInsetsForSafeArea() {
 		guard let cv = collectionView else { return }
-
-		sectionInset = cv.safeAreaInsets
+		
+		var insets = baseSectionInset + cv.safeAreaInsets
 
 		switch scrollDirection {
 		case .horizontal:
-			sectionInset.left = 0
-			sectionInset.right = 0
+			insets.left = 0
+			insets.right = 0
 
 		case .vertical:
-			sectionInset.top = 0
-			sectionInset.bottom = 0
+			insets.top = 0
+			insets.bottom = 0
 
 		@unknown default:
-			sectionInset = .zero
+			insets = .zero
 		}
+		
+		self.sectionInset = insets
 	}
 
 	override func prepare() {
