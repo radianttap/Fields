63286d5e831181f84a95dde71197948f8a00439e
Aleksandar Vacić <av@radianttap.com>
2020-11-03 19:20:41 +0100

    Improved chances that label contents will not be shortened.

--- Fields/Cells/FormFieldCell.swift
+++ Fields/Cells/FormFieldCell.swift
@@ -13,6 +13,9 @@ class FormFieldCell: UICollectionViewCell {
 	override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
 		let attr = layoutAttributes.copy() as! UICollectionViewLayoutAttributes
 
+		let labels: [UILabel] = deepGrepSubviews()
+		labels.forEach { $0.preferredMaxLayoutWidth = min($0.preferredMaxLayoutWidth, bounds.width) }
+		
 		if #available(iOS 13, *) {
 			//	works fine
 		} else {
@@ -21,9 +24,29 @@ class FormFieldCell: UICollectionViewCell {
 		}
 
 		let fittedSize = systemLayoutSizeFitting(UIView.layoutFittingCompressedSize,
-												 withHorizontalFittingPriority: UILayoutPriority.fittingSizeLevel,
+												 withHorizontalFittingPriority: UILayoutPriority.required,
 												 verticalFittingPriority: UILayoutPriority.fittingSizeLevel)
 		attr.frame.size.height = ceil(fittedSize.height)
 		return attr
+	}
+}
+
+fileprivate extension UIView {
+	func grepSubviews<T: UIView>() -> [T] {
+		return subviews.compactMap { $0 as? T }
+	}
+	
+	func deepGrepSubviews<T: UIView>() -> [T] {
+		var arr: [T] = grepSubviews()
+		
+		let children: [T] = subviews.reduce([]) { current, v in
+			var arr = current
+			let x: [T] = v.deepGrepSubviews()
+			arr.append(contentsOf: x)
+			return arr
+		}
+		
+		arr.append(contentsOf: children)
+		return arr
 	}
 }
