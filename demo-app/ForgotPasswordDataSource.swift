40aba6c1c4cc000d988b81e50210b678da4bf40f
Aleksandar Vacić <av@radianttap.com>
2024-09-30 10:32:04 +0200

    Cleanup

--- demo-app/ForgotPasswordDataSource.swift
+++ demo-app/ForgotPasswordDataSource.swift
@@ -1,57 +1,60 @@
 import Foundation
 
+@MainActor
 final class ForgotPasswordDataSource: NSObject {
 	//    Dependencies
-
+	
 	weak var controller: ForgotPasswordController? {
 		didSet { prepareView() }
 	}
-
+	
 	init(user: User?) {
 		self.email = user?.username
 	}
-
-
+	
+	
 	//  Data model
-
+	
 	private var email: String?
 	private(set) var error: Error?
-
+	
 	private(set) lazy var emailModel: TextFieldModel = makeEmailFormModel()
-
+	
 	//  MARK:- Form Fields
-
+	
 	/// Possible form fields
 	enum FieldId: String {
 		case email
 	}
-
+	
 	/// Dictionary of errors to show, per field.
 	private var fieldErrors: [FieldId: String] = [:]
 }
 
 private extension ForgotPasswordDataSource {
 	func prepareView() {}
-
+	
 	func renderContentUpdates() {
 		controller?.renderContentUpdates()
 	}
-
+	
 	func makeEmailFormModel() -> TextFieldModel {
-		let model = TextFieldModel(id: FieldId.email.rawValue,
-								   title: NSLocalizedString("Email (username)", comment: ""),
-								   value: email)
+		let model = TextFieldModel(
+			id: FieldId.email.rawValue,
+			title: NSLocalizedString("Email (username)", comment: ""),
+			value: email
+		)
 		model.customSetup = { textField in
 			textField.textContentType = .emailAddress
 			textField.keyboardType = .emailAddress
 		}
 		model.valueChanged = {
 			[weak self] string, _ in
-
+			
 			self?.email = string
 			model.value = string
 		}
 		return model
 	}
-
+	
 }
--- demo-app/LoginController.swift
+++ demo-app/LoginController.swift
@@ -1,12 +1,12 @@
 import UIKit
 
 final class LoginController: FieldsCollectionController {
-
+	
 	//	MARK:- View lifecycle
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+		
 		setupUI()
 		applyTheme()
 	}
@@ -14,41 +14,41 @@ final class LoginController: FieldsCollectionController {
 
 private extension LoginController {
 	//	MARK:- Internal
-
+	
 	func applyTheme() {
 		view.backgroundColor = UIColor(hex: "EBEBEB")
 	}
-
+	
 	func setupUI() {
 		title = "Login"
-
+		
 		navigationItem.leftBarButtonItem = {
 			let bbi = UIBarButtonItem(title: NSLocalizedString("Sign up", comment: ""), style: .plain, target: self, action: #selector(openAccount))
 			return bbi
 		}()
-
+		
 		collectionView.delegate = self
 	}
-
+	
 	func prepare(_ dataSource: LoginDataSource?) {
 		dataSource?.controller = self
 	}
-
+	
 	func render(_ dataSource: LoginDataSource?) {
 		collectionView.reloadData()
 	}
-
+	
 	//	MARK:- Actions
-
+	
 	@objc func openAccount(_ sender: UIBarButtonItem) {
 		guard
 			let ds = dataSource as? LoginDataSource,
 			let user = ds.user
 		else { return }
-
+		
 		let vc = RegisterController()
 		vc.dataSource = RegisterDataSource(user)
-
+		
 		show(vc, sender: self)
 	}
 }
@@ -58,17 +58,17 @@ extension LoginController: UICollectionViewDelegate {
 		guard
 			let ds = dataSource as? LoginDataSource
 		else { return }
-
-		let field = ds.field(at: indexPath)
-
-		switch field.id {
-		case LoginDataSource.FieldId.forgotpassword.rawValue:
-			let vc = ForgotPasswordController.instantiate()
-			vc.dataSource = ForgotPasswordDataSource(user: ds.user)
-			show(vc, sender: self)
-
-		default:
-			break
+		
+		let fieldId = ds.field(at: indexPath)
+		
+		switch fieldId {
+			case .forgotpassword:
+				let vc = ForgotPasswordController.instantiate()
+				vc.dataSource = ForgotPasswordDataSource(user: ds.user)
+				show(vc, sender: self)
+				
+			default:
+				break
 		}
 	}
 }
