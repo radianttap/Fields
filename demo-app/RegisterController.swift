0ed408ea0865d0e5e1966ba97b14bf23c5fdf74e
Aleksandar Vacić <av@radianttap.com>
2024-09-30 11:14:04 +0200

    Cleanup

--- demo-app/RegisterController.swift
+++ demo-app/RegisterController.swift
@@ -23,39 +23,3 @@ private extension RegisterController {
 	func setupUI() {
 	}
 }
-
-
-/*
-extension RegisterController: UICollectionViewDelegateFlowLayout {
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-		guard
-			let layout = collectionViewLayout as? FieldHeightSizingLayout,
-			let section = dataSource?.section(at: indexPath.section),
-			let field = dataSource?.field(at: indexPath)
-		else { return .zero }
-
-		var itemSize = layout.itemSize
-
-		switch field.id {
-		case RegisterDataSource.FieldId.postcode.rawValue, RegisterDataSource.FieldId.country.rawValue,
-			 RegisterDataSource.FieldId.billingPostcode.rawValue, RegisterDataSource.FieldId.billingCountry.rawValue:
-			itemSize.width *= 1 / addressColumnSplit
-
-		case RegisterDataSource.FieldId.city.rawValue, RegisterDataSource.FieldId.billingCity.rawValue:
-			itemSize.width *= min((1 / addressColumnSplit) * 2, 1)
-
-		case RegisterDataSource.FieldId.title.rawValue:
-			itemSize.width *= 1 / personTitleColumnSplit
-
-		case RegisterDataSource.FieldId.firstName.rawValue:
-			itemSize.width *= max(1, personTitleColumnSplit - 1) / personTitleColumnSplit
-
-		default:
-			break
-		}
-
-		return itemSize
-	}
-}
-
-*/
